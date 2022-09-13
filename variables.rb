084545c4621fea6a721d20bc3c6727d89b0b6e88
Alex Pitzel <pitzelalex@gmail.com>
2022-09-13 16:47:00 -0400

    Add puts statements

--- variables.rb
+++ variables.rb
@@ -9,3 +9,19 @@ meme = 69.420
 is_trolling = true
 is_tired = true
 is_quitting = false
+
+## Reassign
+name = "Alexander Pitzel"
+email = "hotwheels357@hotmail.com"
+
+puts name
+puts email
+puts city
+puts age
+puts day
+puts year
+puts price
+puts meme
+puts is_trolling
+puts is_tired
+puts is_quitting
